8375a663573714e76d60f317e05fc6a07ded8074
le10x <leanderdrb@gmail.com>
2026-04-21 11:41:20 -0400

    Refactor PlayLayer initialization and label creation

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,21 @@ using namespace geode::prelude;
 
 class $modify(PlayLayer) {
     bool init(GJGameLevel* level, bool useReplay, bool dontSave) {
-        if (!PlayLayer::init(level, useReplay, dontSave)) return false;
+    if (!PlayLayer::init(level, useReplay, dontSave)) return false;
 
-        std::string miTexto = Mod::get()->getSettingValue<std::string>("mi-texto-personalizado");
+    std::string miTexto = Mod::get()->getSettingValue<std::string>("mi-texto-personalizado");
 
-        // MDTextArea soporta etiquetas de color automáticamente
-        auto label = MDTextArea::create(miTexto, {200.f, 50.f});
-        
-        auto winSize = CCDirector::sharedDirector()->getWinSize();
-        label->setPosition({winSize.width / 2, winSize.height - 30.f});
-        
-        // Esto quita el fondo oscuro que trae por defecto
-        label->getBGSprite()->setVisible(false); 
-        
-        this->addChild(label, 100);
-
-        return true;
+    // TextArea nativo: usa "chatFont.fnt" para que los colores funcionen bien
+    auto label = TextArea::create(miTexto, "chatFont.fnt", 1.0f, 300.f, {0.5f, 0.5f}, 20.f, false);
+    
+    auto winSize = CCDirector::sharedDirector()->getWinSize();
+    label->setPosition({winSize.width / 2, winSize.height - 30.f});
+    
+    // Aquí sí funciona getBGSprite() en la mayoría de versiones o puedes buscarlo:
+    if (label->getBGSprite()) {
+        label->getBGSprite()->setVisible(false);
     }
-};
+
+    this->addChild(label, 100);
+    return true;
+}
